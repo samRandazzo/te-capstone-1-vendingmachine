ef3b9f3daedeb47a05e80c752d0346dd907828e3
Arthur Mueller <AWM21@pitt.edu>
2019-02-08 10:22:22 -0500

    Somehow restarting my computer fixed the problem

--- src/main/java/com/techelevator/view/Menu.java
+++ src/main/java/com/techelevator/view/Menu.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import com.techelevator.*;
 
 public class Menu {
-	//We have to make a list of everything in our Vending machine
+	// We have to make a list of everything in our Vending machine
 	static LinkedHashMap<String, Consumable> itemMap = new LinkedHashMap<String, Consumable>();
 	static Scanner input = new Scanner(System.in);
 
@@ -28,9 +28,10 @@ public class Menu {
 					double price = Double.parseDouble(lineSplit[2]);
 					String type = lineSplit[3];
 					int numberOfItems = 5;
-					
-					//Pretty sure one of those 'Case' things would work here but I can't figure out how to do that
-					
+
+					// Pretty sure one of those 'Case' things would work here but I can't figure out
+					// how to do that
+
 					if (type.equals("Gum")) {
 						Consumable item = new Gum(product, price, numberOfItems);
 						itemMap.put(location, item);
@@ -58,25 +59,24 @@ public class Menu {
 
 	public static void mainMenu() {
 		boolean stop = false;
-		
+
 		while (!stop) {
 
 			System.out.println("(1) Display Vending Machine Items\n(2) Purchase\n(3) Restock Machine");
 			int selection = Integer.parseInt(input.nextLine());
-			if (selection==1) {
+			if (selection == 1) {
 				displayItems();
 				System.out.println();
 
-			} else if (selection==2) {
+			} else if (selection == 2) {
 				purchase();
 				System.out.println();
 
-			} else if (selection==3) {
+			} else if (selection == 3) {
 				stop = true;
 			}
 
 		}
-		
 
 	}
 
@@ -95,37 +95,42 @@ public class Menu {
 
 	public static void purchase() {
 		double balance = 0.00;
-		System.out.println(
-				"(1) Feed Money\n(2) Select Product\n(3) Finish Transaction\nCurrent Money Provided: $" + balance);
-		
-		int selection = Integer.parseInt(input.nextLine());
-		if (selection==1) {
-			balance = feedMoney(balance);
-			System.out.println("Your balance is: $" + balance);
-		} else if (selection==2) {
-			System.out.println("Enter product location: ");
-			String location = input.nextLine();
-			Consumable item = itemMap.get(location);
-			if (item.getPrice() <= balance && item.getNumberOfItems() > 0) {
-				purchaseProduct(item, balance);
-				item.oneLessItem();
+		boolean stop = false;
 
+		while (!stop) {
+			System.out.println(
+					"(1) Feed Money\n(2) Select Product\n(3) Finish Transaction\nCurrent Money Provided: $" + balance);
+
+			int selection = Integer.parseInt(input.nextLine());
+			if (selection == 1) {
+				balance = feedMoney(balance);
+				System.out.println("Your balance is: $" + balance);
+			} else if (selection == 2) {
+				System.out.println("Enter product location: ");
+				String location = input.nextLine();
+				Consumable item = itemMap.get(location);
+				if (item.getPrice() <= balance && item.getNumberOfItems() > 0) {
+					balance =purchaseProduct(item, balance);
+					item.oneLessItem();
+				}else {
+					System.out.println("Sorry, we could not complete your purchase.");
+				}
+
+			} else if (selection == 3) {
+				finishTransaction(balance);
+				balance = 0.00;
+				stop=true;
 			}
-
-		} else if (selection==3) {
-			finishTransaction(balance);
-			balance = 0.00;
 		}
-		
 
 	}
 
 	private static double feedMoney(double balance) {
 
 		System.out.println("How much money would you like to insert?");
-		
+
 		balance += Integer.parseInt(input.nextLine());
-		input.close();
+
 		return balance;
 	}
 
@@ -144,4 +149,5 @@ public class Menu {
 	public static void restockMachine() {
 
 	}
+
 }
