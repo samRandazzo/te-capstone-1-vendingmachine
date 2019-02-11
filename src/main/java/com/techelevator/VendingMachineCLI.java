20c5f70e8d20be66047a2c132bdfd484b201ba38
Arthur Mueller <AWM21@pitt.edu>
2019-02-11 08:55:47 -0500

    added comments

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -7,7 +7,17 @@ import com.techelevator.view.Menu;
 public class VendingMachineCLI {
 
 	public static void main(String[] args) {
+		/*
+		 * Stock Machine loads the input file and gets the machine ready to be used.
+		 * Stock Machine runs automatically when the program starts.
+		 */
 		Menu.stockMachine();
+		/*
+		 * Main Menu runs immediately after Stock Machine is done.
+		 * Main Menu loops so users can continually use the program.
+		 * Main Menu can be manually exited.
+		 * Exiting Main Menu ends the program and creates the sales report.
+		 */
 		Menu.mainMenu();
 	}
 }
